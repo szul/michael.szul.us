381bfefe3bb97a9d9e7c34ac3bf23e5bdfe8ed33
szul <michael@szul.us>
2015-04-16 16:48:52 -0400

    Changes to pathing for static files

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 #!/usr/bin/env node
 
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
 
 /*
  * Serve static content.
  */
-app.use('/', express.static(__dirname + '/static')); 
+var publicdir = __dirname + '/static';
+
+app.use(function(req, res, next) {
+  if (req.path.indexOf('.') === -1) {
+    var file = publicdir + req.path + '.html';
+    fs.exists(file, function(exists) {
+      if (exists)
+        req.url += '.html';
+      next();
+    });
+  }
+  else
+    next();
+});
+app.use(express.static(publicdir));
 
 /*
  *For Azure deployment
