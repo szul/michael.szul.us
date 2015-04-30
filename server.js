48e41840fd07e7e6183ec7750381acb91e6931c0
szul <michael@szul.us>
2015-04-30 07:57:13 -0400

    Removed blog items

--- server.js
+++ server.js
@@ -2,11 +2,8 @@
 
 var express = require('express');
 var fs = require('fs');
-var cheerio = require('cheerio');
 
 var app = express();
-app.set('views', '/views')
-app.set('view engine', 'jade')
  
 /*
  * Serve static content.
@@ -27,25 +24,6 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static(publicdir));
-
-app.get('/blog-posts/', function (req, res) {
-
-  fs.readdir(publicdir + '/blog', function(err, files) {
-    files.filter(function(file) {
-      return file.substr(-5) === '.html';
-    }).forEach(function(file) {
-      if (file !== 'index.html') {
-        fs.readFile(file, 'utf-8', function(err, contents) {
-          $ = cheerio.load(contents);
-          
-        });
-      }
-    });
-  });
-
-  res.json({  });
-
-});
 
 /*
  *For Azure deployment
