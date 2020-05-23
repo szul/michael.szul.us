6b6286cad87eb552707d8bb8d99bf2c4e3aaa4a5
Michael Szul <michael@szul.us>
2020-05-23 09:32:05 -0400

    Added milligrid

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ const concat = require("gulp-concat");
 gulp.task('styles', () => {
     return gulp.src([
         './node_modules/normalize.css/normalize.css',
-        './node_modules/milligram/dist/milligram.min.css'
+        './node_modules/milligram/dist/milligram.min.css',
+        './node_modules/milligrid/dist/Milligrid.css',
     ])
         .pipe(concat('concat.css'))
         .pipe(gulp.dest('./static/css/_gen/'));
